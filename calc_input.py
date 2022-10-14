ecb157269abce1a281e0a150721ec9d1f9a051bf
Denis.Karev <goofy.adun@gmail.com>
2022-10-14 16:56:15 +0300

    еще изменения. но на сегодня хватит

--- calc_input.py
+++ calc_input.py
@@ -1,18 +1,19 @@
 from cheks import menu_check
 
-def menu_inp(val: str):
-    res = False
-    while not res:
+def menu_inp(val: str) -> int:
+    """ Меню верхнего уровня """
+    res = ' '
+    while res not in val:
         res = menu_check(input('Введите выбор:'))
     else:
         return int(res)
 
-def real_inp(val: str):
-    # check
+def real_inp(val: str) -> int:
+    ### check
     inp = float(input('Введите выбор:'))
     return inp
 
-def complex_inp(val: str):
+def complex_inp(val: str) -> int:
     # check
     inp = complex(input('Введите выбор:'))
     return inp

b1b4cca61da99c20765ddb4fafbf6479ca7929ee
ars <ars9000@gmail.com>
2022-10-18 17:36:40 +0300

    контороллер завершон

--- controller.py
+++ controller.py
@@ -10,47 +10,42 @@ import model_mod as rem
 import model_div_ as div_
 import model_sqrt as sqrt
 
-
-
-# all_num = "0123456789.,"
-
-
-def user_num(start: int):  # ars# для понимания
+def user_num(start: int):               # ars# для понимания
     if start == 1:                      # что запрашиваем используем переменную
-
-        return real_inp()  # цифра берется из выбора первого меню
-    else:
-        # если 1 то рациональные если 2 то комплексные
-        return complex_inp()
-
+        return real_inp()               # цифра берется из выбора первого меню
+    elif start == 2:
+        return complex_inp()            # если 1 то рациональные если 2 то комплексные
+    elif start == 0:
+        exit()
+        
 
 def run():
-    draw_menu_start()
-
-    in_run = menu_inp('012')                # первое меню
-
-    if in_run == 1:
-        draw_menu_real()                    # меню реальных чисел # возможный выбор 012345678
-        in_start = menu_inp('012345678')
-        operation(in_run, in_start)
-    elif in_run == 2:
-        draw_menu_complex()                 # меню комплексных чисел # возможный выбор 0123456
-        in_start = menu_inp('0123456')
-        operation(in_run, in_start)
-    else:
-        exit()
-
+    while True:
+        draw_menu_start()
+        in_run = menu_inp('012')                # первое меню
+        if in_run == 1:
+            draw_menu_real()                    # меню реальных чисел # возможный выбор 012345678
+            in_start = menu_inp('012345678')
+            operation(in_run, in_start)
+        elif in_run == 2:
+            draw_menu_complex()                 # меню комплексных чисел # возможный выбор 0123456
+            in_start = menu_inp('0123456')
+            operation(in_run, in_start)
+        else:
+            exit()
 
 def operation_2num(tip_num: int, oper, znak):
     num1 = user_num(tip_num)  # запрашиваем 1е число
     num2 = user_num(tip_num)  # запрашиваем 2е число
     result = oper.init(num1, num2)  # фиксируем результат
     # loger(nam1,znak,nam2,result)    #отправляем в логер
-
     return result
-    # return (result)
 
-
+def operation_1num(tip_num: int, oper, znak):
+    num1 = user_num(tip_num)  # запрашиваем 1е число
+    result = oper.init(num1)  # фиксируем результат
+    # loger(nam1,znak,result)    #отправляем в логер
+    return result
 
 def operation(tip_num: int, tip_oper: int):
     if tip_oper == 1:  # Отрисовывается результать сложения первого и второго числа для обоих случиев
@@ -62,11 +57,11 @@ def operation(tip_num: int, tip_oper: int):
     elif tip_oper == 3:  # Отрисовывается результать перемножения первого и второго числа для обоих случиев
         draw_result(operation_2num(tip_num, mult, "*"))
     elif tip_oper == 4:  # Отрисовывается результать деления
-
         draw_result(operation_2num(tip_num, div, "/"))  # модуль деления
     elif tip_oper == 5:
         if tip_num == 1:
-            draw_result(operation_2num(tip_num, div_, "//"))  # модуль целочисленного деления #
+            # модуль целочисленного деления #
+            draw_result(operation_2num(tip_num, div_, "//"))
         else:
             # модуль возведения в степень #
             draw_result(operation_2num(tip_num, pow, "**"))
@@ -75,13 +70,13 @@ def operation(tip_num: int, tip_oper: int):
             # модуль остатка от деления #
             draw_result(operation_2num(tip_num, rem, "%"))
         else:
-            draw_result(operation_2num(tip_num, sqrt, "sqrt"))  # тут требуется одно число !!!! это типа важно 
-                                                                # модуль квадратного корня #
+            # модуль квадратного корня #
+            draw_result(operation_1num(tip_num, sqrt, "sqrt"))
     elif tip_oper == 7:
         # модуль возведения в сепень #
         draw_result(operation_2num(tip_num, pow, "**"))
-    elif tip_oper == 8:         
-        draw_result(operation_2num(tip_num, sqrt, "sqrt"))      # тут требуется одно число !!!! это типа важно
-                                                                # модуль квадратного корня #
+    elif tip_oper == 8:
+        # модуль квадратного корня #
+        draw_result(operation_1num(tip_num, sqrt, "sqrt"))
     elif tip_oper == 0:         # переход в предыдущие меню
         run()
